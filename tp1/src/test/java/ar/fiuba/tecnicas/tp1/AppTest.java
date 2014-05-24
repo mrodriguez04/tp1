6fcc50ed2091763e3ebec4847821a334d71ce84b
nicolascoco85 <nicolascoco85@yahoo.com.ar>
2014-05-24 11:40:23 -0300

    primeros test

--- tp1/src/test/java/ar/fiuba/tecnicas/tp1/AppTest.java
+++ tp1/src/test/java/ar/fiuba/tecnicas/tp1/AppTest.java
@@ -31,8 +31,9 @@ public class AppTest
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    public void testTiempo()
     {
-        assertTrue( true );
+      	
+        assertEquals( true, true );
     }
 }
