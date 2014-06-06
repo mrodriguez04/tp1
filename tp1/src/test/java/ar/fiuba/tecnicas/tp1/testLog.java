ba969cd20502716f3ec810722c08c296ee093437
nicolascoco85 <nicolascoco85@yahoo.com.ar>
2014-06-06 13:43:08 -0300

    Se agregan test para probar multiples loguers

--- tp1/src/test/java/ar/fiuba/tecnicas/tp1/testLog.java
+++ tp1/src/test/java/ar/fiuba/tecnicas/tp1/testLog.java
@@ -7,11 +7,25 @@ public class testLog extends TestCase {
 
 	public void testLoguerNoExistente(){
 		
-		Logeable log = Logeable.getInstance("moduloA");
+		Logeable log = Logeable.getInstance("moduloB");
 		
 		assertEquals(log.get_Nombre(),"loguer default");
 	}
 	
+	public void testCambiaNombreLoguerYSiInstancioOtraIgualMantieneSuNombreCambiado(){
+		
+		Logeable log = Logeable.getInstance("moduloA");
+		log.cambiar_Nombre("pepe");
+		Logeable log2 = Logeable.getInstance("moduloA");
+		assertEquals(log2.get_Nombre(),"pepe");
+	}
+	
+	public void testInfo(){
+		
+		Logeable log = Logeable.getInstance("moduloB");
+		log.info("TODO BIEN");
+		assertEquals(true,true);
+	}
 
 	
 }
